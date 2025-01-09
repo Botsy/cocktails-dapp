0803d59e2c475ae26dd83519de6e2def312228a8
Botzy <bodim93@gmail.com>
2025-01-09 10:55:49 +0200

    chore(dependencies): Added Wagmi with initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
+import { WagmiProvider } from 'wagmi';
 import PageWrapper from '@components/common/page-wrapper';
 import { Provider as ChakraUiProvider } from '@components/ui/provider';
 import Router from './Router';
+import { config } from '../wagmi.config';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,16 +22,18 @@ const queryClient = new QueryClient({
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <BrowserRouter>
-        <ChakraUiProvider>
-          <PageWrapper>
-            <Router />
-          </PageWrapper>
-        </ChakraUiProvider>
-      </BrowserRouter>
-    </QueryClientProvider>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <BrowserRouter>
+          <ChakraUiProvider>
+            <PageWrapper>
+              <Router />
+            </PageWrapper>
+          </ChakraUiProvider>
+        </BrowserRouter>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
 
